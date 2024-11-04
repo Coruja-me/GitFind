30d7ce9a1192481fdacecc223b702c88da0ef548
Coruja <cauavalves@gmail.com>
2024-11-04 18:22:41 -0300

    Pt. 3

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,12 +8,21 @@ function App() {
       <Header />
       <div className="content">
         <img src={background} className="bg" alt="Background app" />
-        <div className="info">
-          <div>
+        <main className="info">
+          <section>
             <input name="user" placeholder="@username" />
             <button>BUSCAR</button>
-          </div>
-        </div>
+          </section>
+          <section className="perfil">
+            <img src="https://avatars.githubusercontent.com/u/178964215?v=4" className="profile" alt="Foto de perfil" />
+            <article>
+              <h3>Cauã Vieira Alves</h3>
+              <h4>@Coruja-me</h4>
+              <p>Descrição</p>
+            </article>
+          </section>
+          <hr />
+        </main>
       </div>
     </div>
   );
