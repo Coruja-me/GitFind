6a45b51e390af9a0fa49ecda7f04013566d57f5b
Coruja <cauavalves@gmail.com>
2024-11-04 17:40:53 -0300

    pt.2

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,20 @@
 import { Header } from "../../Components/Header";
+import background from "../../Assets/bg.png";
+import "./styles.css"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content">
+        <img src={background} className="bg" alt="Background app" />
+        <div className="info">
+          <div>
+            <input name="user" placeholder="@username" />
+            <button>BUSCAR</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
